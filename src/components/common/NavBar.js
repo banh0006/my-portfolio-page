7a8a16f3bf5d11d4b18e1653b9d718be8a7f2885
banh0006 <banh0006@algonquinlive.com>
2020-09-03 20:36:39 -0400

    add nav bar

---
+++ src/components/common/NavBar.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import "../../css/NavBar.css"
+import {
+    Navbar,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+export default function NavBar ( props ) {
+    const [show, setShow] = useState({ ...props.showNav })
+    const styles = {
+        navLink: {
+            'backgroundColor': 'red'
+        },
+        navBrand: {
+            'listStyle': 'none'
+        },
+    }
+
+    useEffect(() => {
+        console.log(props.showNav)
+        setShow(props.showNav)
+    }, [props.showNav])
+
+    return (
+        <React.Fragment>
+            <Navbar color="light" fixed="top" className={show === true ? 'active' : 'hidden'}>
+                <NavItem style={styles.navBrand}>Thien Banh</NavItem>
+                <Nav className="ml-auto">
+                    <NavItem style={styles.navLink}>
+                        <NavLink href="/">Profile</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/">Skills</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/">Work</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/">Contact</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </React.Fragment>
+    )
+}
