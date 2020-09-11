58978dec507b98f095a6a190af67847410e2e1dc
banh0006 <banh0006@algonquinlive.com>
2020-09-11 10:02:19 -0400

    added intro text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
-import NavBar from "./components/common/NavBar";
+import React, { useState, useEffect } from "react"
+import NavBar from "./components/common/NavBar"
 import ParallaxImage from './components/ParallaxImage'
 import AboutPage from './components/pages/AboutPage'
 import WorkPage from './components/pages/WorkPage'
 import ContactPage from './components/pages/ContactPage'
 
-import MessengerCustomerChat from "react-messenger-customer-chat";
+import MessengerCustomerChat from "react-messenger-customer-chat"
 import { Scrollspy } from 'reactstrap-scrollspy'
 
 function App() {
   
-  const [showNav, setShowNav] = useState(true);
+  const [showNav, setShowNav] = useState(true)
 
   const handleScroll = () => {
-    const currentPosition = window.pageYOffset;
+    const currentPosition = window.pageYOffset
     const navbar = document.getElementsByClassName("navbar")[0]
     const headerHeight = navbar.clientHeight
 
@@ -24,22 +24,22 @@ function App() {
     }
 
     // show/hide navbar when scroll
-    // const [scrolledPosition, setSrolledPosition] = useState(0);
+    // const [scrolledPosition, setSrolledPosition] = useState(0)
     // if (scrolledPosition > currentPosition) {
-    //   setShowNav(true);
+    //   setShowNav(true)
     // } else {
-    //   setShowNav(false);
+    //   setShowNav(false)
     // }
 
-    // setSrolledPosition(currentPosition);
-  };
+    // setSrolledPosition(currentPosition)
+  }
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll)
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
+      window.removeEventListener("scroll", handleScroll)
+    }
+  })
 
   return (
     <>
@@ -52,13 +52,10 @@ function App() {
         <AboutPage />
         <WorkPage />
         <ContactPage />
-        <div>
-          
-        </div>
         <MessengerCustomerChat pageId="116543563511206" appId="667569657298462" minimized={false} shouldShowDialog={true}/>
       </Scrollspy>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -2,114 +2,41 @@ import React, { useState, useEffect } from "react";
 import "../../css/AboutPage.css"
 
 export default function AboutPage() {
+  const handleScroll = () => {
+    const curve = document.querySelector(".curve")
+    const homeDivHeight = document.getElementById("home").clientHeight;
+    let currentScroll = 1 + (window.scrollY - homeDivHeight)/-500
+
+    curve.style.transform = `scaleY(${currentScroll})`
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  })
+
   return (
     <div id="about">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet
-      consectetur adipiscing elit. Eget mi proin sed libero. Quis imperdiet
-      massa tincidunt nunc pulvinar sapien. Dictum non consectetur a erat nam at
-      lectus urna duis. Ut venenatis tellus in metus vulputate eu. Nec dui nunc
-      mattis enim ut tellus elementum sagittis. Arcu cursus vitae congue mauris
-      rhoncus aenean. Suspendisse in est ante in. Magna sit amet purus gravida
-      quis blandit turpis cursus. Aliquet eget sit amet tellus cras adipiscing.
-      In iaculis nunc sed augue. Donec enim diam vulputate ut. Vel pretium
-      lectus quam id leo in vitae turpis. Varius duis at consectetur lorem donec
-      massa sapien. Scelerisque eleifend donec pretium vulputate. Ultrices
-      gravida dictum fusce ut placerat orci. Sollicitudin tempor id eu nisl nunc
-      mi ipsum faucibus. Molestie a iaculis at erat. Aliquet enim dolor sit amet
-      consectetur adipiscing elit. Erat velit scelerisque in dictum non
-      consectetur a erat nam. Sit amet risus nullam eget felis eget nunc
-      lobortis. Sit amet justo donec enim diam. Ut morbi tincidunt augue
-      interdum. Eget est lorem ipsum dolor sit amet. Placerat vestibulum lectus
-      mauris ultrices eros in cursus turpis massa. Et tortor at risus viverra
-      adipiscing at in tellus integer. Morbi leo urna molestie at elementum eu.
-      Libero volutpat sed cras ornare. Vestibulum lectus mauris ultrices eros in
-      cursus. Egestas maecenas pharetra convallis posuere. Turpis massa sed
-      elementum tempus egestas sed sed risus. In ornare quam viverra orci. Sit
-      amet venenatis urna cursus eget nunc scelerisque. Pellentesque habitant
-      morbi tristique senectus et netus. Mi in nulla posuere sollicitudin
-      aliquam ultrices sagittis orci a. Vitae congue.In pretium lectus quam id
-      leo in vitae turpis. Varius duis at consectetur lorem donec massa sapien.
-      Scelerisque eleifend donec pretium vulputate. Ultrices gravida dictum
-      fusce ut placerat orci. Sollicitudin tempor id eu nisl nunc mi ipsum
-      faucibus. Molestie a iaculis at erat. Aliquet enim dolor sit amet
-      consectetur adipiscing elit. Erat velit scelerisque in dictum non
-      consectetur a erat nam. Sit amet risus nullam eget felis eget nunc
-      lobortis. Sit amet justo donec enim diam. Ut morbi tincidunt augue
-      interdum. Eget est lorem ipsum dolor sit amet. Placerat vestibulum lectus
-      mauris ultrices eros in cursus turpis massa. Et tortor at risus viverra
-      adipiscing at in tellus integer. Morbi leo urna molestie at elementum eu.
-      Libero volutpat sed cras ornare. Vestibulum lectus mauris ultrices eros in
-      cursus. Egestas maecenas pharetra convallis posuere. Turpis massa sed
-      elementum tempus egestas sed sed risus. In ornare quam viverra orci. Sit
-      amet venenatis urna cursus eget nunc scelerisque. Pellentesque habitant
-      morbi tristique senectus et netus. Mi in nulla posuere sollicitudin
-      aliquam ultrices sagittis orci a. Vitae congueIn iaculis nunc sed augue.
-      Donec enim diam vulputate ut. Vel pretium lectus quam id leo in vitae
-      turpis. Varius duis at consectetur lorem donec massa sapien. Scelerisque
-      eleifend donec pretium vulputate. Ultrices gravida dictum fusce ut
-      placerat orci. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus.
-      Molestie a iaculis at erat. Aliquet enim dolor sit amet consectetur
-      adipiscing elit. Erat velit scelerisque in dictum non consectetur a erat
-      nam. Sit amet risus nullam eget felis eget nunc lobortis. Sit amet justo
-      donec enim diam. Ut morbi tincidunt augue interdum. Eget est lorem ipsum
-      dolor sit amet. Placerat vestibulum lectus mauris ultrices eros in cursus
-      turpis massa. Et tortor at risus viverra adipiscing at in tellus integer.
-      Morbi leo urna molestie at elementum eu. Libero volutpat sed cras ornare.
-      Vestibulum lectus mauris ultrices eros in cursus. Egestas maecenas
-      pharetra convallis posuere. Turpis massa sed elementum tempus egestas sed
-      sed risus. In ornare quam viverra orci. Sit amet venenatis urna cursus
-      eget nunc scelerisque. Pellentesque habitant morbi tristique senectus et
-      netus. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-      Vitae congueIn iaculis nunc sed augue. Donec enim diam vulputate ut. Vel
-      pretium lectus quam id leo in vitae turpis. Varius duis at consectetur
-      lorem donec massa sapien. Scelerisque eleifend donec pretium vulputate.
-      Ultrices gravida dictum fusce ut placerat orci. Sollicitudin tempor id eu
-      nisl nunc mi ipsum faucibus. Molestie a iaculis at erat. Aliquet enim
-      dolor sit amet consectetur adipiscing elit. Erat velit scelerisque in
-      dictum non consectetur a erat nam. Sit amet risus nullam eget felis eget
-      nunc lobortis. Sit amet justo donec enim diam. Ut morbi tincidunt augue
-      interdum. Eget est lorem ipsum dolor sit amet. Placerat vestibulum lectus
-      mauris ultrices eros in cursus turpis massa. Et tortor at risus viverra
-      adipiscing at in tellus integer. Morbi leo urna molestie at elementum eu.
-      Libero volutpat sed cras ornare. Vestibulum lectus mauris ultrices eros in
-      cursus. Egestas maecenas pharetra convallis posuere. Turpis massa sed
-      elementum tempus egestas sed sed risus. In ornare quam viverra orci. Sit
-      amet venenatis urna cursus eget nunc scelerisque. Pellentesque habitant
-      morbi tristique senectus et netus. Mi in nulla posuere sollicitudin
-      aliquam ultrices sagittis orci a. Vitae congueIn iaculis nunc sed augue.
-      Donec enim diam vulputate ut. Vel pretium lectus quam id leo in vitae
-      turpis. Varius duis at consectetur lorem donec massa sapien. Scelerisque
-      eleifend donec pretium vulputate. Ultrices gravida dictum fusce ut
-      placerat orci. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus.
-      Molestie a iaculis at erat. Aliquet enim dolor sit amet consectetur
-      adipiscing elit. Erat velit scelerisque in dictum non consectetur a erat
-      nam. Sit amet risus nullam eget felis eget nunc lobortis. Sit amet justo
-      donec enim diam. Ut morbi tincidunt augue interdum. Eget est lorem ipsum
-      dolor sit amet. Placerat vestibulum lectus mauris ultrices eros in cursus
-      turpis massa. Et tortor at risus viverra adipiscing at in tellus integer.
-      Morbi leo urna molestie at elementum eu. Libero volutpat sed cras ornare.
-      Vestibulum lectus mauris ultrices eros in cursus. Egestas maecenas
-      pharetra convallis posuere. Turpis massa sed elementum tempus egestas sed
-      sed risus. In ornare quam viverra orci. Sit amet venenatis urna cursus
-      eget nunc scelerisque. Pellentesque habitant morbi tristique senectus et
-      netus. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-      Vitae congueIn pretium lectus quam id leo in vitae turpis. Varius duis at
-      consectetur lorem donec massa sapien. Scelerisque eleifend donec pretium
-      vulputate. Ultrices gravida dictum fusce ut placerat orci. Sollicitudin
-      tempor id eu nisl nunc mi ipsum faucibus. Molestie a iaculis at erat.
-      Aliquet enim dolor sit amet consectetur adipiscing elit. Erat velit
-      scelerisque in dictum non consectetur a erat nam. Sit amet risus nullam
-      eget felis eget nunc lobortis. Sit amet justo donec enim diam. Ut morbi
-      tincidunt augue interdum. Eget est lorem ipsum dolor sit amet. Placerat
-      vestibulum lectus mauris ultrices eros in cursus turpis massa. Et tortor
-      at risus viverra adipiscing at in tellus integer. Morbi leo urna molestie
-      at elementum eu. Libero volutpat sed cras ornare. Vestibulum lectus mauris
-      ultrices eros in cursus. Egestas maecenas pharetra convallis posuere.
-      Turpis massa sed elementum tempus egestas sed sed risus. In ornare quam
-      viverra orci. Sit amet venenatis urna cursus eget nunc scelerisque.
-      Pellentesque habitant morbi tristique senectus et netus. Mi in nulla
-      posuere sollicitudin aliquam ultrices sagittis orci a. Vitae congue
+      <section>
+        <div className="my-intro">
+            <p className="intro-title">About me</p>
+            <p className="intro-header">Hello, my name is Thien</p>
+            <p className="intro-body">I am a Fullstack web developer specialized in React. 
+              With 6 years of learning and working experiences in IT, I have a diverse set of skills in both 
+              front-end and back-end development, ranging from Java/Node.js, to React + Redux, 
+              all the way to Javascript + HTML + CSS, UI UX design. I am passionate about technology. 
+              I love learning new technologies, fixing little details, and optimizing apps.
+            </p>
+          </div>
+        <span className="curve">
+          <img src={process.env.PUBLIC_URL + '/images/white-curve.png'} />
+        </span>
+      </section>
+      <div className="my-skills">
+          <p className="skills-title">Skills</p>
+      </div>
     </div>
   );
 }
