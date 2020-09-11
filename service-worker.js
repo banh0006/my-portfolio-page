6fa5a12600879a3b47fa6bb88827a2f4bfd69454
banh0006 <banh0006@algonquinlive.com>
2020-09-11 10:02:54 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e78bdbe6a1fb24a3b6000563daf210f.js"
+  "/precache-manifest.c1614dcf8e1c9462353c57e2e3ec5606.js"
 );
 
 self.addEventListener('message', (event) => {
