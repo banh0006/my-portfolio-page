97f29d90f68b85feb2a9b0a90ff494d8adf29253
banh0006 <banh0006@algonquinlive.com>
2020-09-15 20:47:19 -0400

    Updates

---
+++ precache-manifest.87947c8061dfb2f9c1d4be247e0ee90a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2e1da44d16fbd29b7862498fc0c979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b43682fd6a8acc1136f",
+    "url": "/static/css/2.2e0fc591.chunk.css"
+  },
+  {
+    "revision": "2c085ab8a12fd7b87da3",
+    "url": "/static/css/main.9428c457.chunk.css"
+  },
+  {
+    "revision": "4b43682fd6a8acc1136f",
+    "url": "/static/js/2.6be6b90d.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.6be6b90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c085ab8a12fd7b87da3",
+    "url": "/static/js/main.2035e76f.chunk.js"
+  },
+  {
+    "revision": "1ca39a5947ba7d055d7f",
+    "url": "/static/js/runtime-main.6847859b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fa4c56e970c9f05636873c986420767.js"
+  "/precache-manifest.87947c8061dfb2f9c1d4be247e0ee90a.js"
 );
 
 self.addEventListener('message', (event) => {
